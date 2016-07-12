0dd30d8ea93014fca585a2f6a2da29e698d0723e
Jason Wuerch <wuerchjason@gmail.com>
2016-07-12 18:23:20 +0300

    create 3 more instances of movie class

--- entertainment_center.py
+++ entertainment_center.py
@@ -14,6 +14,21 @@ school_of_rock = media.Movie("School of Rock",
                             "A crazy musician leads schoolchildren to the wonders of music",
                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
                             "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
-school_of_rock.show_trailer()
+
+ratatouille = media.Movie("Ratatouille",
+                          "A rat is a chef in Paris",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "Going back in time to meet authors",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_poster.jpg",
+                                "https://www.youtube.com/watch?v=atLg2wQQxvU")
+
+hunger_games = media.Movie("Hunger Games",
+                           "A really real reality show",
+                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "https://www.youtube.com/watch?v=PbA63a7H0bo")
+
 
                      
